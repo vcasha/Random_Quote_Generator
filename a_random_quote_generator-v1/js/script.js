d8708c1db3c96cc09090d74c6509d768fdc9a18b
Vincent Casha <vincent.casha@gmail.com>
2018-12-31 14:20:02 -0600

    Generated Completed Project

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -6,46 +6,110 @@ FSJS project 1 - A Random Quote Generator
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended: 
-    - Add at least one `year` and/or `citation` property to at least one 
-      quote object.
+/***
+These is my array of quote objects that will be used to pass the necessary information for displaying the properties of the object.
 ***/
+var quotes = [
+  {
+    quote: 'Do you still believe in Santa Claus? I hear it is marginal at your age',
+    source: 'Donald J. Trump',
+    citation: 'Christmas Eve phone calls as NORAD monitors Santa Claus',
+    year: '2018'
+
+  },
+  {
+    quote: 'I\'m too drunk to taste <i>this</i> chicken',
+    source: 'Will Ferrel as Ricky Bobby quoting Colonel Sanders',
+    citation: 'Talledega Nights',
+    year: '2006'
+
+  },
+  {
+    quote: 'Lieutenant Dan, ice cream!',
+    source: 'Tom Hanks as Forrest Gump',
+    //citation: 'Forrest Gump',
+    year: '1994'
+
+  },
+  {
+    quote: 'It\'s a purse, <i>K!!</i>',
+    source: 'Ken Jeong as Mr.Chow',
+    citation: 'The Hangover, Part I',
+    //year: '2009'
+
+  },
+  {
+    quote: 'Peeing your pants is cool!',
+    source: 'Adam Sandler as Billy Madison',
+    citation: 'Billy Madison',
+    year: '1995'
+
+  }
+
+]
 
 
 
 
 /***
-  Create the `getRandomQuote` function to:
-   - generate a random number 
-   - use the random number to `return` a random quote object from the 
-     `quotes` array.
+  This function will generate a random number use that number to identify the index of the
+  quote object we will display. The function then returns the array with the index.
 ***/
+
+function getRandomQuote(quotes){
+  var randomNumber = Math.ceil(Math.random() * 4);
+  return quotes[randomNumber];
+
+
+}
 
 
 
 
 /***
-  Create the `printQuote` function to: 
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
+  This function executes the RandomQuote Function and stores it in the quoteProperties variable. The quotePoperties variable
+  is now storing one of the objects from the quotes array. Since the quote and source properties are required
+  we can store those in the message variable as an HTML string right away. But with the citation and year properties being optional, we
+  can run conditional statements to first check if citation and year are both properties that exist in the object, if so we will add the HTML string for these properties
+  to the message variable and print it. If neither are there, we then check for each object individually and if present, add the content to the HTML string and print. Lastly, if neither of the optional properties
+  are present we will add the closing p tag and print the message with only the two required properties.
+  ***/
+
+function printQuote (){
+  var quoteProperties = getRandomQuote(quotes);
+  var message = '<p class="quote">' + quoteProperties.quote +  '</p>';
+  message += '<p class="source">'+ quoteProperties.source
+  if(quoteProperties.hasOwnProperty('citation') && quoteProperties.hasOwnProperty('year')) {
+    message += '<span class="citation">' + quoteProperties.citation
+    message += '</span>' + '<span class="year">' + quoteProperties.year + '</span>' + '</p>';
+    var outputDiv = document.getElementById('quote-box');
+    outputDiv.innerHTML = message;
+
+  } else if (quoteProperties.hasOwnProperty('citation')){
+      message += '<span class="citation">' + quoteProperties.citation;
+      var outputDiv = document.getElementById('quote-box');
+      outputDiv.innerHTML = message;
+
+  } else if (quoteProperties.hasOwnProperty('year')){
+      message += '</span>' + '<span class="year">' + quoteProperties.year + '</span>' + '</p>';
+      var outputDiv = document.getElementById('quote-box');
+      outputDiv.innerHTML = message;
+
+
+}   else{
+      message += '</p>';
+      var outputDiv = document.getElementById('quote-box')
+      outputDiv.innerHTML = message;
+}
+}
 
 
 
 
 /***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
+  When the "Show another quote" button is clicked, the event listener
+  below will be triggered, and it will call, or "invoke", the `printQuote`
+  function. So do not make any changes to the line of code below this
   comment.
 ***/
 
